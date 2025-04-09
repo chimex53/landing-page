cdc3ded18c235ee887ce3787d3cbe76e49b60150
chimex53 <chimex53@gmail.com>
2025-04-09 04:14:25 +0100

    added some transitions

--- src/landing page/Menu/Menu.jsx
+++ src/landing page/Menu/Menu.jsx
@@ -1,5 +1,8 @@
+import React from "react";
+import { motion } from "framer-motion";
 import styles from "./Menu.module.css";
 import productMenu from "./Data";
+
 const Menu = () => {
   return (
     <div>
@@ -9,12 +12,23 @@ const Menu = () => {
         </div>
         <div className={styles.menuContainer}>
           {productMenu.map((item) => (
-            <div key={item.id} className={styles.menuItems}>
-              {" "}
+            <motion.div
+              key={item.id}
+              className={styles.menuItems}
+              initial={{ opacity: 0, y: 30 }} // Start below and invisible
+              whileInView={{ opacity: 1, y: 0 }} // Animate to visible and to original position
+              transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 25,
+                delay: item.id * 0.1, // Add a slight delay for each item
+              }}
+              viewport={{ once: true }} // Only trigger the animation once when the element comes into view
+            >
               <img src={item.Image} alt={item.title} />
               <h3>{item.title}</h3>
               <p>{item.description}</p>
-            </div>
+            </motion.div>
           ))}
         </div>
       </section>
